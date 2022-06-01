8abe9725e46b34e86f64f287c991e6b92aa5dd4e
alexissoko <alesoko@live.com>
2022-06-01 12:04:36 -0300

    making data dinamycal from views to template

--- charts/views.py
+++ charts/views.py
@@ -5,15 +5,16 @@ from .forms import *
 import pandas as pd
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import json
 
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.utils import timezone
 
 # Create your views here.
 from django.views.generic import TemplateView
-# from chartjs.views.lines import BaseLineChartView
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
+# from chartjs.views.lines import BaseLineChartView
 
 
 
@@ -23,21 +24,14 @@ def home(request):
 @login_required
 def reporting(request):
     if request.GET.get("begin"):
-        print("begin")
-        print(request.GET.get("begin"))
         mesh = request.GET.get("begin")
-
         sales = Sale.objects.filter(date__range=[mesh, timezone.now().strftime('%Y-%m-%d')]).order_by("-date")
     else:
-        print("Ouch")
-        print(request)
         sales = Sale.objects.all().order_by("-date")
 
     all_data = {prod.invoice.name: {} for prod in sales}
-
     df_labels = sorted([x[0].strftime('%Y-%m-%d') for x in sales.values_list("date").distinct()])
 
-    sales.values().order_by("-date")
     for sale in sales:
         all_data[sale.invoice.name][sale.date.strftime('%Y-%m-%d')] = sale.quantity
     
@@ -48,8 +42,15 @@ def reporting(request):
     
     for k,v in all_data.items():
         all_data[k] = [all_data[k][x] for x in sorted(all_data[k])]
+
+    print("df_labels")
+    print(df_labels)
     print("all_data")
+    print(all_data)
     labels = list(all_data.keys())
+    values = list(all_data.values())
+    
+
 
     mydict= {
         'sales':sales,
@@ -58,10 +59,7 @@ def reporting(request):
         "df1" : sorted({x["date"].strftime('%Y-%m-%d'): x["quantity"] for x in sales.values().order_by("-date") if x["invoice_id"] == 1}.items()),
         "df_labels" : df_labels,
         "labels" : labels,
-        # "df1" : sorted({x["date"].strftime('%Y-%m-%d'): x["quantity"] for x in sales.values().order_by("-date") if x["invoice_id"] == 1}.items()),
-        # "df2" : sorted({x["date"].strftime('%Y-%m-%d'): x["quantity"] for x in sales.values().order_by("-date") if x["invoice_id"] == 2}.items()),
-        "df1" : all_data["Mil hojas"],
-        "df2" : all_data["Flan casero"]
+        "values" : values,
     
     }
     return render(request, 'index.html', context=mydict)
