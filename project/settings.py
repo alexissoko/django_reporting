bf006dfb93dcc845e4ddae884d32e273e2dc7b4d
alexissoko <alesoko@live.com>
2022-06-05 13:29:26 -0300

    making data dinamycal from views to templateush

--- project/settings.py
+++ project/settings.py
@@ -14,8 +14,11 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+other_BASE_DIR = Path(__file__).resolve()
 
-
+print("BASE_DIR")
+print(BASE_DIR)
+print(other_BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
